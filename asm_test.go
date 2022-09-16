1a8ce74e738e0fb45ac5777e36542c5ba262a18d
liuqiang <liuqiang.06@bytedance.com>
2022-09-16 18:36:23 +0800

    refactor

--- asm_test.go
+++ asm_test.go
@@ -7,27 +7,27 @@ import (
 func BenchmarkFloat64(b *testing.B) {
 	var f float64 = 1.0
     for i := 0; i < b.N; i++ { 
-		__f64toa(nil, f)
+		__f64(nil, f)
 	}
 }
 
 func BenchmarkFloat32(b *testing.B) {
 	var f float32 = 1.0
     for i := 0; i < b.N; i++ { 
-		__f32toa(nil, f)
+		__f32(nil, f)
 	}
 }
 
 func BenchmarkInt32(b *testing.B) {
 	var f int32 = 1
     for i := 0; i < b.N; i++ { 
-		__i32toa(nil, f)
+		__i32(nil, f)
 	}
 }
 
 func BenchmarkInt64(b *testing.B) {
 	var f int64 = 1
     for i := 0; i < b.N; i++ { 
-		__i64toa(nil, f)
+		__i64(nil, f)
 	}
 }
--- export.go
+++ export.go
@@ -3,19 +3,19 @@ package asmdemo
 //go:nosplit
 //go:noescape
 //goland:noinspection GoUnusedParameter
-func __f64toa(out *byte, val float64) (ret int)
+func __f64(out *byte, val float64) (ret int)
 
 //go:nosplit
 //go:noescape
 //goland:noinspection GoUnusedParameter
-func __i32toa(out *byte, val int32) (ret int)
+func __i32(out *byte, val int32) (ret int)
 
 //go:nosplit
 //go:noescape
 //goland:noinspection GoUnusedParameter
-func __f32toa(out *byte, val float32) (ret int)
+func __f32(out *byte, val float32) (ret int)
 
 //go:nosplit
 //go:noescape
 //goland:noinspection GoUnusedParameter
-func __i64toa(out *byte, val int64) (ret int)
+func __i64(out *byte, val int64) (ret int)
